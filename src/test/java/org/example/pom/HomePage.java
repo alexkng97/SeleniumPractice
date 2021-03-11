7df32d5d5480c8c599f3426a9cddea65887978d4
Alex Ng <alexkng97@gmail.com>
2021-03-11 15:33:54 +0000

    category getter implemented

--- src/test/java/org/example/pom/HomePage.java
+++ src/test/java/org/example/pom/HomePage.java
@@ -1,11 +1,36 @@
 package org.example.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePage extends Page{
 
+	List<WebElement> cards = new ArrayList<>();
+
 	public HomePage(WebDriver webDriver) {
 		super(webDriver);
+	}
+
+	public WebElement getCategory(String category){
+		WebElement allCategories = webDriver.findElement(By.className("category-cards"));
+
+		String[] individualCategory = allCategories.getText().split("\n");
+		int index = 0;
+		for(int i = 0; i < individualCategory.length; i++){
+			if(individualCategory[i].contains(category)){
+				index = i + 1;
+				break;
+			}
+		}
+
+		WebElement selected = allCategories.findElement(By.cssSelector("div:nth-child(" + index + ")" ));
+		System.out.println(selected.getText());
+		return selected;
+
 	}
 
 
--- src/test/java/org/example/pomtest/HomeTest.java
+++ src/test/java/org/example/pomtest/HomeTest.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class HomeTest {
 	WebDriver webDriver;
@@ -15,7 +19,14 @@ public class HomeTest {
 
 	@BeforeEach
 	void setup(){
-		webDriver = new ChromeDriver();
+		boolean headless = true;
+		if(headless) {
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("headless");
+			webDriver = new ChromeDriver(chromeOptions);
+		}else {
+			webDriver = new ChromeDriver();
+		}
 		homePage = new HomePage(webDriver);
 		webDriver.get(homePage.getBASE_URL());
 
@@ -26,6 +37,13 @@ public class HomeTest {
 	void logoPress() {
 		homePage.clickLogo();
 		Assertions.assertEquals("https://demoqa.com/", homePage.getURL());
+
+	}
+
+	@Test
+	@DisplayName("Initial cards getter")
+	void initialCardsGetter() {
+		homePage.getCategory("Elements");
 
 	}
 }
